74890b1efd4933fcb8cb8c02e028b4a5cb4c0052
alexandr <sasha06122002@gmail.com>
2023-05-14 17:44:35 +0300

    Отрисуй меня полностью (часть 1)

--- js/data.js
+++ js/data.js
@@ -1,4 +1,4 @@
-import { getRandomNumber } from './util.js';
+import { getRandomNumber, isStringInSize } from './util.js';
 
 function createPost(id){
   return {
@@ -10,12 +10,10 @@ function createPost(id){
   };
 }
 
-function createPosts(quantity){
+export function createPosts(quantity){
   const posts = [];
   for (let i = 1; i <= quantity; i++) {
     posts[i-1] = createPost(i);
   }
   return posts;
 }
-
-export { createPosts };
